37f7acf54971bca74f244302505094fb74aa1dbc
themicp <themicp@gmail.com>
2023-10-19 16:32:21 +0300

    Add function that creates receipt proof and submits it to the lightclient

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,38 @@
-import { getEnv } from './utils.js'
+import { bytesToHex, formatReceipt, getEnv } from './utils.js';
 import { EthAPI } from './eth.js';
 import { LightClientAPI } from './lightclient.js';
 import { serializeBlockVerificationData } from './types.js';
-import { toHexString } from '@chainsafe/ssz';
-import * as capella from '@lodestar/types/capella';
+import { TrieWrapper} from './triewrapper.js';
+import { Receipt, UserInput } from './types.js';
+import assert from 'assert';
 
 const CONTRACT_ADDRESS = getEnv("CONTRACT_ADDRESS") 
+
+async function isTopicInTransaction(api: EthAPI, lightClient: LightClientAPI, input: UserInput) {
+	const block = await api.getBlock(input.blockNumber);
+
+	const receipts: Receipt[] = (await api.getBlockTransactionReceipts(input.blockNumber)).map(formatReceipt);
+	const requestedTxReceipt = receipts.find((r) => r.transactionHash === input.transactionHash);
+	if (!requestedTxReceipt) throw new Error(`Could not find receipt for transaction ${input.transactionHash} in block ${input.blockNumber}`);
+
+	const transactionIndex = parseInt(requestedTxReceipt.transactionIndex, 16);
+	const receiptsTrie = await TrieWrapper.trieFromReceipts(receipts);
+	assert(
+		block.receiptsRoot.slice(2) === bytesToHex(receiptsTrie.root()),
+		`Expected receipts root (${block.receiptsRoot}) doesn't match the actual (${bytesToHex(receiptsTrie.root())})`);
+	const proof = await TrieWrapper.createProof(receiptsTrie, transactionIndex);
+
+	const isValidReceipt = true;
+	// const isValidReceipt = await lightClient.verify_receipt(
+	// 	block.receiptsRoot.slice(2),
+	// 	proof.map((p) => bytesToHex(p)),
+	// 	TrieWrapper.encodeKey(transactionIndex)
+	// );
+
+	if (isValidReceipt) {
+		return requestedTxReceipt.logs.find(log => log[1].includes(input.topic)) !== undefined;
+	}
+}
 
 const main = async () => {
 	const api = new EthAPI()
