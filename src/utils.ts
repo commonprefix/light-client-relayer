013749147f4fdb61c3dc17d00a926571a1e78f54
themicp <themicp@gmail.com>
2023-10-19 16:29:55 +0300

    Add helper functions to format receipts and convert bytes to hex

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import "dotenv/config"
+import { Receipt } from "./types"
 
 export const getEnv = (key: string, defaultValue ?: string): string => {
 	if (!process.env[key] && !defaultValue) {
@@ -7,4 +8,19 @@ export const getEnv = (key: string, defaultValue ?: string): string => {
 
 	// @ts-ignore
 	return process.env[key] || defaultValue
+}
+
+export const bytesToHex = (bytes: Uint8Array): string => {
+	return Buffer.from(bytes).toString('hex');
+}
+
+export const formatReceipt = (receipt: any): Receipt => {
+  return {
+    ...receipt,
+    cumulativeBlockGasUsed: parseInt(receipt.cumulativeGasUsed, 16),
+    bitvector: receipt.logsBloom,
+    logs: receipt.logs.map((log: any) => [log.address, log.topics, log.data]),
+    type: parseInt(receipt.type, 16),
+    status: parseInt(receipt.status, 16),
+  };
 }
